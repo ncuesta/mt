5ea18b2f3afb4848b729d4b3eeea8c71cf402239
Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2015-03-09 09:53:33 -0300

    Initial commit

---
+++ calc.rb
@@ -0,0 +1,63 @@
+require 'csv'
+require 'json'
+require 'active_support/all'
+require 'bigdecimal'
+
+today = if ARGV.size > 0
+          Date.parse(ARGV[1])
+        else
+          Date.today
+        end
+
+data_path = './facturacion.csv'
+categories_path = './categorias.json'
+
+class Bill
+  attr_accessor :date, :amount
+
+  def initialize(date = nil, amount = 0)
+    self.date = date
+    self.amount = amount
+  end
+end
+
+class Category
+  attr_accessor :name, :base, :upto, :cost
+
+  def initialize(name, base, upto, cost)
+    self.name = name
+    self.base = base
+    self.upto = upto
+    self.cost = cost
+  end
+end
+
+def load_bills(path)
+  bills = []
+  CSV.foreach(path) do |row|
+    date = Date.parse(row.first)
+    amount = BigDecimal.new(row.last)
+    bills << Bill.new(date, amount)
+  end
+  bills
+end
+
+def load_categories(path)
+  Hash[
+    JSON.parse(File.read(path)).map do |name, data|
+      [data['from']..data['to'], Category.new(name, data['from'], data['to'], data['cost'])]
+    end
+  ]
+end
+
+bills = load_bills(data_path)
+categories = load_categories(categories_path)
+
+a_year_ago = 1.year.ago(today)
+included_bills = bills.select { |b| b.date >= a_year_ago && b.date <= today }
+total = included_bills.map(&:amount).inject(&:+)
+
+puts "Total entre %{start} y %{end}: %{total}" % { start: a_year_ago, end: today, total: total }
+range_and_category = categories.select { |range, data| range.include? total }
+category = range_and_category.first.last
+puts "Categoría: %{name}, costo: $ %{cost}" % { name: category.name, cost: category.cost }
